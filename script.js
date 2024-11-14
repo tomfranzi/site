3f7f303bfb805ec7336418b318a72951e5a3bd14
tomfranzi <tom.franzi@saintmichelannecy.fr>
2024-11-15 00:35:48 +0100

    ok

---
+++ script.js
@@ -0,0 +1,5 @@
+// Animation du texte de bienvenue avec un délai
+document.addEventListener("DOMContentLoaded", () => {
+  const heading = document.querySelector("h1");
+  heading.classList.add("fade-in");
+});
